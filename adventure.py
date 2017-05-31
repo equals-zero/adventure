dbea6ada2eafacde161395bc510334501a33bde6
Silva <svoboda@openmailbox.org>
2017-06-01 00:01:40 +0200

    Ein paar kleinere Änderungen, Fix von "GEGENSTAENDEN", hinzufuegen von "MOEBELN"........

--- adventure.py
+++ adventure.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 from props import *
-from map import GameMap
+from map import GameMap, GameGegenstaende, GameMoebelstuecke
 
 # Ein bescheuertes Textadventure
 
 Inventar = Inventory(132) # Inventar des Spielers
-Schwert = Gegenstand("Superschwinger", "Ein Habsburgerischer Degen.", 1, 200)  # Erzeuge Schwert "Superschwinger"
-Bogen = Gegenstand("Langbogen", "Ein klassischer, Englischer Langbogen.", 2, 120)  # Erzeuge Bogen "Langbogen"
+
 
 # Fuege Gegenstaende in Moebelstueck ein.
-Schachtel = Moebelstueck("Alte Schachtel", "Links grün versifft, sonst wie neu.", 12, [Schwert, Bogen], False, False)
 
 Schachtel.showAllContents()
 
--- map.py
+++ map.py
@@ -3,9 +3,18 @@ Contains the Map of Rooms.
 Hier werden die einzelnen Reaume initialisiert und miteinander verknuepft.
 """
 
-from props import Raum
+from props import Raum, Gegenstand, Moebelstueck
 
 GameMap = [
-    Raum("Gute Stube", "Eine aufgeraeumte Stube, hier wohnt wohl ein Autist.", [2, 3, 7], False, False, False)
-    Raum("Badezimmer", "Der Putz broeckelt von den Waenden.", [1], False, False, False),
+    Raum("Gute Stube", "Eine aufgeraeumte Stube, hier wohnt wohl ein Autist.", [2, 3, 7], False, False, False),
+    Raum("Badezimmer", "Der Putz broeckelt von den Waenden.", [1], False, False, False)
+]
+
+GameGegenstaende = [
+    Gegenstand("Superschwinger", "Ein Habsburgerischer Degen.", 1, 200),
+    Gegenstand("Langbogen", "Ein klassischer, Englischer Langbogen.", 2, 120)
+]
+
+GameMoebelstuecke = [
+    Moebelstueck("Alte Schachtel", "Links grün versifft, sonst wie neu.", 12, [Schwert, Bogen], False, False)
 ]
